45e632809b3a52ff9e331771c10dcfa56523eeca
sebastian barcia <sebastianbarcia@hotmail.com>
2023-02-23 11:45:41 +0100

    condiguracion monaco editor

--- src/components/EditJs.jsx
+++ src/components/EditJs.jsx
@@ -1,10 +1,5 @@
 import React, { useState, useEffect } from "react";
-
-const tools = [
-  { id: 1, title: "HTML" },
-  { id: 2, title: "Javascript" },
-  { id: 3, title: "CSS" },
-];
+import Editor from "@monaco-editor/react"
 
 const EditJs = () => {
   const [createHtml, setCreateHtml] = useState(
@@ -15,6 +10,12 @@ const EditJs = () => {
   const [css, setCss] = useState();
   const [press, setPress] = useState(false);
 
+  const tools = [
+    { id: 1, title: "html" , value: html },
+    { id: 2, title: "javascript"  , value: js },
+    { id: 3, title: "css", value: css },
+  ];
+  
   useEffect(() => {
     if (press === true) {
       setCreateHtml(
@@ -37,25 +38,35 @@ const EditJs = () => {
     }
   }, [html, css, js]);
 
-  function update(e) {
+  function update(item, e) {
+    
     setPress(true);
 
-    e.target.id === "1" && setHtml(e.target.value);   
-    e.target.id === "2" && setJs(e.target.value);
-    e.target.id === "3" && setCss(e.target.value);
+    item.id === 1 && setHtml(e);   
+    item.id === 2 && setJs(e);
+    item.id === 3 && setCss(e);
 
   }
-
+  
   return (
     <>
-      {tools.map((item) => {
-        return (
-          <div key={item.id}>
-            <label>{item.title}</label>
-            <textarea id={item.id} onInput={update}></textarea>
-          </div>
-        );
-      })}
+      {
+        tools.map((item) => {
+          return(
+            <div style={{ display: "flex" , gap:15 }} key={item.id}>
+            <div style={{width:"100%"}} >
+              <Editor
+                height="40vh"
+                defaultLanguage={item.title}
+                onChange={(e) => update(item , e)}
+                defaultValue={item.value}
+                theme= "vs-dark"
+              />
+            </div>
+            </div>
+          )
+        })
+      }  
       <iframe srcDoc={createHtml}></iframe>
     </>
   );
