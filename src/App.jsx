9ebc1a75ffb2cd76f59d483dcdd50cc6d3673f39
sebastian barcia <sebastianbarcia@hotmail.com>
2023-02-23 15:41:32 +0100

    fix js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import EditJs from './components/EditJs'
 
 function App() {
   
-
   return (
     <div className="App">
       <EditJs/>
--- src/components/EditJs.jsx
+++ src/components/EditJs.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Editor from "@monaco-editor/react"
+import Editor from "@monaco-editor/react";
 
 const EditJs = () => {
   const [createHtml, setCreateHtml] = useState(
@@ -11,11 +11,11 @@ const EditJs = () => {
   const [press, setPress] = useState(false);
 
   const tools = [
-    { id: 1, title: "html" , value: html },
-    { id: 2, title: "javascript"  , value: js },
+    { id: 1, title: "html", value: html },
+    { id: 2, title: "javascript", value: js },
     { id: 3, title: "css", value: css },
   ];
-  
+ 
   useEffect(() => {
     if (press === true) {
       setCreateHtml(
@@ -27,10 +27,11 @@ const EditJs = () => {
         </style>
       </head>
      <body>
+     ${html}
         <script>
           ${js}
         </script>
-        ${html}
+      
       </body>
     </html>
    `
@@ -39,36 +40,50 @@ const EditJs = () => {
   }, [html, css, js]);
 
   function update(item, e) {
-    
     setPress(true);
 
-    item.id === 1 && setHtml(e);   
+    item.id === 1 && setHtml(e);
     item.id === 2 && setJs(e);
     item.id === 3 && setCss(e);
-
   }
-  
+
   return (
-    <>
-      {
-        tools.map((item) => {
-          return(
-            <div style={{ display: "flex" , gap:15 }} key={item.id}>
-            <div style={{width:"100%"}} >
-              <Editor
-                height="40vh"
-                defaultLanguage={item.title}
-                onChange={(e) => update(item , e)}
-                defaultValue={item.value}
-                theme= "vs-dark"
-              />
-            </div>
-            </div>
-          )
-        })
-      }  
-      <iframe srcDoc={createHtml}></iframe>
-    </>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        height: "90vh",
+        justifyContent: "center",
+        gap: 15,
+      }}
+    >
+      <iframe
+        style={{
+          backgroundColor: "white",
+          width: "80%",
+          height: "40vh",
+          border: "none",
+        }}
+        srcDoc={createHtml}
+      ></iframe>
+      <div
+        style={{ display: "flex", flexDirection: "row", gap: 5, width: "80%" }}
+      >
+        {tools.map((item) => {
+          return (
+            <Editor
+              height="30vh"
+              key={item.id}
+              defaultLanguage={item.title}
+              onChange={(e) => update(item, e)}
+              defaultValue={item.value}
+              theme="vs-dark"
+            />
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
